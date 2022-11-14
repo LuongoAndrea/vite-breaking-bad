fdf4d01456f7c71263f89a789bf9900cd3848cfb
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-11-15 00:57:19 +0100

    componenti

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <HeaderComponent/>
+  </header>
+  <main>
+    <SearchComponent/>
+    <CharacterList/>
+  </main>
+
+</template>
+
+<script>
+  import axios from 'axios';
+  import CharacterList from './components/CharacterList.vue';
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import SearchComponent from './components/SearchComponent.vue';
+
+  export default {
+    components: { HeaderComponent, CharacterList, SearchComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
